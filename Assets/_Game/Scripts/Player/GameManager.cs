a0333758cc7808b7e6b2058ac3b8a280f7f765ed
Nick Maclean <nicolasimaclean@gmail.com>
2022-11-17 09:20:06 -0600

    moved input code to Scripts folder

---
+++ Assets/_Game/Scripts/Player/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Gummi.Patterns;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    const string DEBUGHeader = "<b>[<color=green>Game Manager</color>]</b> ";
+
+    [HideInInspector] public InputListener input;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        if (Instance != this) return;
+        
+        transform.SetParent(null);
+        DontDestroyOnLoad(gameObject);
+
+        input = GetComponent<InputListener>();
+        if (input == null)
+        {
+            Debug.Log(DEBUGHeader + "No Input Listener found!");
+        }
+    }
+}
